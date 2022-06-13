ae92ea694ecccdea1efc4e7d58165c5fdc96c017
Minh Vu <minhd_vu@yahoo.com>
2022-06-13 00:07:05 -0400

    added hardhat-deploy

---
+++ deploy/01-deploy-greeter.ts
@@ -0,0 +1,13 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+  await greeter.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+};
+export default func;
